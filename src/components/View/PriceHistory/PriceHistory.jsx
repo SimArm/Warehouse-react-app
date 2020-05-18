6eb2e75f2d4f87fab62aa01e926ff9be754cab87
Simas Armolaitis <s.armolaitis@gmail.com>
2020-05-18 17:00:31 +0300

    Highcharts added to history components

--- src/components/View/PriceHistory/PriceHistory.jsx
+++ src/components/View/PriceHistory/PriceHistory.jsx
@@ -3,17 +3,34 @@ import "./PriceHistory.scss";
 import { useParams } from "react-router-dom";
 import { getPriceHistory } from "../../productsCommands/changesHistoryCommands";
 
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
 const PriceHistory = () => {
     const { productId } = useParams();
     const priceHistory = getPriceHistory() || [];
+    const filterredPrice = priceHistory.filter(history => history.ProductId == productId).slice(-5);
+    const newPriceHistory = filterredPrice.map((price) => parseInt(price.NewPrice));
 
+    const options = {
+        title: {
+          text: 'Price History'
+        },
+        series: [{
+          data: newPriceHistory
+        }]
+      }
     return (
         <div>
-            { priceHistory.filter(history => history.ProductId == productId).map((price) => {
+            { filterredPrice.map((price) => {
                 return (
                         <div>Price change from: {price.OldPrice} to: {price.NewPrice} at: {price.ChangeTime}</div>                                            );
                 })
             }
+            <HighchartsReact
+                highcharts={Highcharts}
+                options={options}
+            />
         </div>
     );    
 }
--- src/components/View/QuantityHistory/QuantityHistory.jsx
+++ src/components/View/QuantityHistory/QuantityHistory.jsx
@@ -2,18 +2,34 @@ import React from "react";
 import "./QuantityHistory.scss";
 import { useParams } from "react-router-dom";
 import { getQuantityHistory } from "../../productsCommands/changesHistoryCommands";
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
 const QuantityHistory = () => {
     const { productId } = useParams();
     const quantityHistory = getQuantityHistory() || [];
+    const filterredQuantity = quantityHistory.filter(history => history.ProductId == productId).slice(-5);
+    const newQuantityHistory = filterredQuantity.map((quantity) => parseInt(quantity.NewQuantity));
 
+    const options = {
+        title: {
+          text: 'Quantity History'
+        },
+        series: [{
+          data: newQuantityHistory
+        }]
+      }
     return (
         <div>
-            { quantityHistory.filter(history => history.ProductId == productId).map((quantity) => {
+            { filterredQuantity.map((quantity) => {
                 return (
                     <div>Quantity change from: {quantity.OldQuantity} to: {quantity.NewQuantity} at: {quantity.ChangeTime}</div>                                            );
                 })
             }
+            <HighchartsReact
+                highcharts={Highcharts}
+                options={options}
+            />
         </div>
     );    
 }
